4fad35667056494fab65065797307dc37ed0ee42
Arpitha V <arpitha.v@walmart.com>
2022-04-30 15:08:00 +0530

    Added Swagger, Added support for POST API

--- src/main/java/com/myretail/productdescriptionservice/controller/ProductDescriptionController.java
+++ src/main/java/com/myretail/productdescriptionservice/controller/ProductDescriptionController.java
@@ -1,26 +1,77 @@
 package com.myretail.productdescriptionservice.controller;
 
 import com.myretail.productdescriptionservice.models.ProductDescription;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/productdescription")
 public class ProductDescriptionController {
 
-    @RequestMapping("/{productId}")
-    public ProductDescription getProduct(@PathVariable("productId") String productId) {
+    private static Map<String, String> productDescMap;
 
-        Map<String, String> productDescMap = new HashMap<String, String>();
+    @PostConstruct
+    public void init() {
+        productDescMap = new HashMap<>();
         productDescMap.put("1", "IPhone 13");
         productDescMap.put("2", "Organic Jaggery");
         productDescMap.put("3", "Diapers");
         productDescMap.put("4", "Sugarcane Juice");
+    }
 
-        return new ProductDescription(productId, productDescMap.get(productId));
+    @GetMapping
+    @ApiOperation("Fetches all the product descriptions")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Successful Retrieval of Product Description")
+            }
+    )
+    public ResponseEntity<List<ProductDescription>> getAllProducts() {
+        return new ResponseEntity<>(productDescMap.entrySet().stream()
+                .map(entry -> new ProductDescription(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{productId}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("Fetches the product description for a given product ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Successful Retrieval of Product Description")
+            }
+    )
+    public ResponseEntity<ProductDescription> getProduct(@PathVariable("productId") String productId) {
+        if (productDescMap.containsKey(productId)) {
+            return new ResponseEntity<>(
+                    new ProductDescription(productId, productDescMap.get(productId)),
+                    HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping()
+    @ApiOperation("Updates/Creates the product description for a given product ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Successful update of Product Description")
+            }
+    )
+    public ResponseEntity<ProductDescription> addProduct(@RequestBody ProductDescription productDescription) {
+        productDescMap.put(productDescription.getId(), productDescription.getName());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/myretail/productdescriptionservice/models/ProductDescription.java
+++ src/main/java/com/myretail/productdescriptionservice/models/ProductDescription.java
@@ -1,29 +1,15 @@
 package com.myretail.productdescriptionservice.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDescription {
 
     private String id;
-
     private String name;
 
-    public ProductDescription(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
